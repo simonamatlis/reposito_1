2d1bcffaf114b61976d319d3e71fba2e52927ee9
solsejas <ssejas@udesa.edu.ar>
2023-11-17 13:36:40 -0300

    modificando js

--- js/search-results.js
+++ js/search-results.js
@@ -9,38 +9,20 @@ document.addEventListener("DOMContentLoaded", function () {
       .then(response => {
         const peliculas_element = document.getElementById('Resultados');
         const peliculas = response.results;
+        
         if (peliculas.length === 0) {
-          const mensaje = document.createElement('h3');
-          mensaje.textContent = 'No se encontraron resultados';
-          peliculas_element.appendChild(mensaje);
-        }
-  
-        peliculas.forEach(pelicula => {
-          const link = document.createElement('a');
-          link.href = `detallespeli.html?id=${pelicula.id}`;
-          const pelicula_div = document.createElement('div');
-          pelicula_div.classList.add('peli-preview');
-  
-          const pelicula_titulo = document.createElement('h4');
-          pelicula_titulo.textContent = pelicula.title;
-          pelicula_titulo.classList.add('peli-preview-titulo');
-  
-          const pelicula_poster = document.createElement('img');
-          pelicula_poster.src = `https://image.tmdb.org/t/p/w500${pelicula.poster_path}`;
-          pelicula_poster.alt = pelicula.title;
-          pelicula_poster.classList.add('peli-preview-poster');
-  
-          const pelicula_release = document.createElement('h6');
-          pelicula_release.textContent = pelicula.release_date;
-  
-          pelicula_div.appendChild(pelicula_titulo);
-          pelicula_div.appendChild(pelicula_poster);
-          pelicula_div.appendChild(pelicula_release);
-  
-          link.appendChild(pelicula_div);
-  
-          peliculas_element.appendChild(link);
-        });
+        }peliculas_element.innerHTML = '<h3>No se encontraro resultados</h3>';
+          else {
+            peliculas_element.innerHTML = peliculas.map(pelicula => `
+          <a href=""detallespeli.html?id=${pelicula.id}"> 
+            <div class="peli-preview">
+              <h4 class="peli-preview-titulo">${pelicula.title}</h4>
+              <img src= "https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="${pelicula.title}" class="peli-preview-poster">
+              <h6>${pelicula.release_date}</h6>
+            </div>
+          </a>
+          `).join('');
+          }
       })
       .catch(err => console.error(err));
   });
