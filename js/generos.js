3093921639826cb30f34d472b5ec3461a343d31d
solsejas <ssejas@udesa.edu.ar>
2023-11-17 13:49:26 -0300

    generos y search results

--- js/generos.js
+++ js/generos.js
@@ -6,47 +6,29 @@ fetch(`https://api.themoviedb.org/3/genre/movie/list?language=es&api_key=${APIKe
         const generos_element = document.getElementById('GenerosPeliculas');
         const generos = response.genres;
 
-        generos.forEach(genero => {
-            const genero_li = document.createElement('li');
+        generos_element.innerHTML = generos.map(genero => `
+        <li>
+            <a href="detallesgenero.html?id=${genero.id}&tipo=pelicula&nombre=${genero.name}">
+                <h3>${genero.name}</h3>
+            </a>
+        </li>
+        `).join('');
 
-            const genero_link = document.createElement('a');
-            genero_link.href = `detallesgenero.html?id=${genero.id}&tipo=pelicula&nombre=${genero.name}`;
-
-            const genero_titulo = document.createElement('h3');
-            genero_titulo.textContent = genero.name;
-
-            genero_link.appendChild(genero_titulo);
-
-            genero_li.appendChild(genero_link);
-
-            generos_element.appendChild(genero_li);
-        });
     })
-    .catch(err => console.error(err));
+        .catch(err => console.error(err));
 
-fetch(`https://api.themoviedb.org/3/genre/tv/list?language=es&api_key=${APIKey}`)
-    .then(response => response.json())
-    .then(response=> {
-        const generos_element = document.getElementById('GenerosSeries');
-        const generos = response.genres;
-
-        generos.forEach(genero => {
-            const genero_li = document.createElement('li');
-
-            const genero_link = document.createElement('a');
-            genero_link.href = `detallesgenero.html?id=${genero.id}&tipo=serie&nombre=${genero.name}`;
-
-            const genero_titulo = document.createElement('h3');
-            genero_titulo.textContent = genero.name;
-
-            genero_link.appendChild(genero_titulo);
-
-            genero_li.appendChild(genero_link);
-
-            generos_element.appendChild(genero_li);
-        });
-    })
-
-    .catch(function (error) {
-        console.log(error);
-        });
+        fetch(`https://api.themoviedb.org/3/genre/tv/list?language=es&api_key=${APIKey}`)
+        .then(response => response.json())
+        .then(response => {
+          const generos_element = document.getElementById('GenerosSeries');
+          const generos = response.genres;       
+        
+          generos_element.innerHTML = generos.map(genero => `
+          <li>
+              <a href="detallesgenero.html?id=${genero.id}&tipo=serie&nombre=${genero.name}">
+                  <h3>${genero.name}</h3>
+              </a>
+          </li>
+          `).join('');
+        })
+        .catch(err => console.error(err));
--- js/search-results.js
+++ js/search-results.js
@@ -11,18 +11,18 @@ document.addEventListener("DOMContentLoaded", function () {
         const peliculas = response.results;
         
         if (peliculas.length === 0) {
-        }peliculas_element.innerHTML = '<h3>No se encontraro resultados</h3>';
-          else {
+        peliculas_element.innerHTML = '<h3>No se encontraro resultados</h3>';
+        } else {
             peliculas_element.innerHTML = peliculas.map(pelicula => `
-          <a href=""detallespeli.html?id=${pelicula.id}"> 
-            <div class="peli-preview">
-              <h4 class="peli-preview-titulo">${pelicula.title}</h4>
-              <img src= "https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="${pelicula.title}" class="peli-preview-poster">
-              <h6>${pelicula.release_date}</h6>
-            </div>
-          </a>
-          `).join('');
+      <a href=""detallespeli.html?id=${pelicula.id}"> 
+        <div class="peli-preview">
+            <h4 class="peli-preview-titulo">${pelicula.title}</h4>
+            <img src= "https://image.tmdb.org/t/p/w500${pelicula.poster_path}" alt="${pelicula.title}" class="peli-preview-poster">
+            <h6>${pelicula.release_date}</h6>
+        </div>
+      </a>
+    `).join('');
           }
-      })
-      .catch(err => console.error(err));
-  });
+    })
+    .catch(err => console.error(err));
+});
